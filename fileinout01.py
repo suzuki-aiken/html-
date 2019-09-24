d22da95c3536c916b3bcce1adb41e9663519a07b
suzuki-ai <karikakko10@gmail.com>
2019-09-24 17:44:12 +0900

    ファイルの入出力について
    with :closeを呼び出してくれる
    w書き込みモード
    r読み取りモード
    a追記モード

---
+++ fileinout01.py
@@ -0,0 +1,19 @@
+with open('sample01.txt', 'w') as f:
+    f.write('hello\n')
+    f.write('hello')
+
+"""
+f= open('sample01.txt', mode='w')
+
+f.write('hello')
+
+f.close()
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
